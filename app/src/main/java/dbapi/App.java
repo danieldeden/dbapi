1a21eaa2d6a074e191094e3e4856dd7035a452fe
Daniel Deden <daniel.deden@bnearit.se>
2023-05-29 15:56:01 +0200

    testing snakeyaml

--- app/src/main/java/dbapi/App.java
+++ app/src/main/java/dbapi/App.java
@@ -3,12 +3,32 @@
  */
 package dbapi;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        InputStream input = new FileInputStream("input.yaml");
+        Map<String, Object> obj = (Map) yaml.load(input);
+        Map<String, Object> schemas = (Map) obj.get("schemas");
+        System.out.println(schemas.keySet());
+        List<String> l = (List) obj.get("test2");
+        System.out.println(obj);
+        System.out.println(l.get(0));
+        System.out.println(l.get(1));
+
+        Yaml yaml2 = new Yaml(new Constructor(Customer.class));
+        InputStream input2 = new FileInputStream("input1.yaml");
+        Customer obj2 = (Customer) yaml2.load(input2);
+        System.out.println(obj2.name);
+
     }
 }
---
+++ app/src/main/java/dbapi/Customer.java
@@ -0,0 +1,15 @@
+package dbapi;
+
+public class Customer {
+    public String name;
+    public int age;
+
+    public Customer() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
